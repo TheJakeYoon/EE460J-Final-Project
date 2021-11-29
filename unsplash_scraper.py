bbcf13615bda3b51e2e247a215b620ae5647a527
kliment-slice <kliment@utexas.edu>
2021-11-29 11:23:31 -0600

    API scraper of small unsplash images
    Downloads 1393 images tagged cyberpunk

---
+++ unsplash_scraper.py
@@ -0,0 +1,12 @@
+import requests as requests
+import urllib.request as url
+
+r = requests.get('https://api.unsplash.com/search/photos?query=cyberpunk&page=1&per_page=50&client_id=pCvNl3DTJC_Qo54DuCkqrvHXO7WC8sPTCfTzOUfwF8Y')
+data = r.json()
+data.keys() # dict_keys(['total', 'total_pages', 'results'])
+data['total'] # 1393 images
+
+for img in data['results']:
+    filename = "./unsplash/" + str(img['id']) + ".jpg"
+    imgurl = img['urls']['small']
+    url.urlretrieve(imgurl, filename)
