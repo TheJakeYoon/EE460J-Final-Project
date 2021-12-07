164384c6e6cc3d59e9ac1df8581840ee559a94f1
kliment-slice <kliment@utexas.edu>
2021-12-07 16:20:15 -0600

    output metrics

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ data_path = './unsplash/'
 clip = Clippy(label, data_path)
 df_clip = clip.process()
 plot2 = clip.plot()
-style_img_open = Image.open(data_path + df_clip.iloc[0,:].name)
+style_img_open = Image.open(data_path + df_clip.iloc[3,:].name)
 
 #style_img_open = Image.open('./unsplash/470eBDOc8bk.jpg')
 plot2 = content.show(style_img_open, 'Style image of choice')
@@ -36,11 +36,12 @@ plot2 = content.show(style_img_open, 'Style image of choice')
 # resize the chosen style_image to content_image size
 resize = Resize(style_img_open, content_img_open)
 style_img_resized, content_img_resized = resize.resized_tensors()
-metrics = Metrics(style_img_resized, content_img_resized)
-distance = metrics.mse()
-similarity = metrics.ssim()
-print(f'MSE:{distance}, SSIM:{similarity}')
-print(distance*similarity)
+# input vs style metrics
+# metrics = Metrics(style_img_resized, content_img_resized)
+# distance = metrics.mse()
+# similarity = metrics.ssim()
+# print(f'MSE:{distance}, SSIM:{similarity}')
+# print(distance*similarity)
 
 assert style_img_resized.shape == content_img_resized.shape, \
         "Dimension mismatch: style and content images are of different size"
@@ -55,5 +56,10 @@ cyberpunk_img, style_loss_list, content_loss_list = transfer.run()
 show_output = transfer.show_img()
 
 plot5 = transfer.loss_plot()
+
+output_metrics = Metrics(cyberpunk_img, content_img_resized)
+output_distance = output_metrics.mse()
+output_similarity = output_metrics.ssim()
+print(f'Output\nMSE:{output_distance}, SSIM:{output_similarity}')
 
 # utils.save_image(cyberpunk_img, 'cyberpunk.png')
