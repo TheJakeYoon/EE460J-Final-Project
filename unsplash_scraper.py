28c1c476ba18914178c38a8a489e6532db55c9e0
kliment-slice <kliment@utexas.edu>
2021-12-07 15:46:32 -0600

    API pull loop

--- unsplash_scraper.py
+++ unsplash_scraper.py
@@ -1,12 +1,13 @@
 import requests as requests
 import urllib.request as url
 
-r = requests.get('https://api.unsplash.com/search/photos?query=cyberpunk&page=1&per_page=50&client_id=pCvNl3DTJC_Qo54DuCkqrvHXO7WC8sPTCfTzOUfwF8Y')
-data = r.json()
-data.keys() # dict_keys(['total', 'total_pages', 'results'])
-data['total'] # 1393 images
+for i in range(50):
+    r = requests.get(f'https://api.unsplash.com/search/photos?query=cyberpunk&page={i}&per_page=50&client_id=pCvNl3DTJC_Qo54DuCkqrvHXO7WC8sPTCfTzOUfwF8Y')
+    data = r.json()
+    data.keys() # dict_keys(['total', 'total_pages', 'results'])
+    data['total'] # 1393 images
 
-for img in data['results']:
-    filename = "./unsplash/" + str(img['id']) + ".jpg"
-    imgurl = img['urls']['small']
-    url.urlretrieve(imgurl, filename)
+    for img in data['results']:
+        filename = "./unsplash/" + str(img['id']) + ".jpg"
+        imgurl = img['urls']['small']
+        url.urlretrieve(imgurl, filename)
